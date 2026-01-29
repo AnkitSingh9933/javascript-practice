f14d76504e62b467dc20f830b4a9c8b96b2860e0
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-01-29 09:03:52 +0530

    added basic example for this keyword

---
+++ dom/objects/this-keyword/this-basic.js
@@ -0,0 +1,7 @@
+/*
+What I learned today:
+- 'this' refers to the object that is calling the function.
+- In the global scope, 'this' refers to the window object.
+*/
+
+console.log(this); // window (in browser)
