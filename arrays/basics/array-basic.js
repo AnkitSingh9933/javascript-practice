7606a326decbe51e11ed040e09fe5d21e3e0a29a
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-02-14 21:30:30 +0530

    added basic array example

---
+++ arrays/basics/array-basic.js
@@ -0,0 +1,15 @@
+/*
+
+Topic: JavaScript Array Basics
+
+What I learned today:
+- Array stores multiple values in a single variable.
+- Index starts from 0.
+- Array can store different data types.
+*/
+
+var subjects = ["Hindi", "English", "Math"];
+
+console.log(subjects);
+console.log(subjects[0]);  // Hindi
+console.log(subjects.length);  // 3
