79ead0362f18a4ff7925d3447bd0275c626d7ccb
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-02-19 19:48:48 +0530

    added basic indexOf() array method example

---
+++ arrays/methods/indexOf-basic.js
@@ -0,0 +1,17 @@
+/*
+
+Topic: Array indexOf()
+
+What I learned today:
+- indexOf() returns the index of an element.
+- If element is not found, it returns -1.
+- Index starts from 0.
+*/
+
+var fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+var mangoIndex = fruits.indexOf("Mango");
+console.log("Mango index:", mangoIndex);
+
+var grapeIndex = fruits.indexOf("Grapes");
+console.log("Grapes index:", grapeIndex);
