1667b8ed0e32172fa7bc1c904692b12353752e98
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-03-08 09:32:04 +0530

    added basic string example

---
+++ 📄 strings/basics/string-basic.js
@@ -0,0 +1,13 @@
+/*
+Topic: JavaScript String Basics
+
+What I learned today:
+- String is used to store text.
+- Strings can be written using "", '' or backticks.
+*/
+
+var name = "Ankit";
+var city = 'Kolkata';
+
+console.log(name);
+console.log(city);
