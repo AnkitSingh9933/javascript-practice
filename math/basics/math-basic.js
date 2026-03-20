e2de3fe1a70f78bb0b2fb874a51f8482a5cfc633
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-03-20 10:25:22 +0530

    added basic math object example

---
+++ math/basics/math-basic.js
@@ -0,0 +1,11 @@
+/*
+
+Topic: Math Object Basics
+
+What I learned today:
+- Math is a built-in object.
+- It provides mathematical operations.
+*/
+
+console.log(Math.PI);
+console.log(Math.sqrt(16));
