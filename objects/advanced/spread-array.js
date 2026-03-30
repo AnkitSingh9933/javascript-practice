aedf7dd1ac89e7752263430a6e5d7008e6fbe5ab
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-03-30 08:48:32 +0530

    added spread operator example for array copy

---
+++ objects/advanced/spread-array.js
@@ -0,0 +1,16 @@
+/*
+Topic: Spread Operator (Array Copy)
+*/
+
+// Code
+var arr = [1, 2, 3];
+
+var newArr = [...arr];
+
+console.log(newArr);
+
+// Output
+// [1, 2, 3]
+
+// Reason
+// Spread copies all elements into new array
