508f76e223f85fb4657764fc1b86b38cdfa82299
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-04-04 08:36:14 +0530

    added array destructuring example

---
+++ objects/advanced/array-destructuring.js
@@ -0,0 +1,19 @@
+/*
+
+Topic: Array Destructuring
+*/
+
+// Code
+var colors = ["Red", "Blue", "Green"];
+
+var [first, second] = colors;
+
+console.log(first);
+console.log(second);
+
+// Output
+// Red
+// Blue
+
+// Reason
+// Values are assigned based on index position
