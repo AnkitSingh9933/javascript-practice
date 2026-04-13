767f1444d012408e5d8477315090bbced05156bf
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-04-13 13:19:35 +0530

    added multiline string example using template literals

---
+++ 📄 strings/advance/template-literals-multiline.js
@@ -0,0 +1,19 @@
+/*
+
+Topic: Template Literals (Multiline)
+*/
+
+// Code
+var text = `Hello
+This is line 2
+This is line 3`;
+
+console.log(text);
+
+// Output
+// Hello
+// This is line 2
+// This is line 3
+
+// Reason
+// Backticks allow multiline strings without \n
