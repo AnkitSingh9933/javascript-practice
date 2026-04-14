f13b7b5976fff47630cab17fee1a97b8ac5d219c
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-04-14 09:13:44 +0530

    added expression example in template literals

---
+++ 📄 strings/advance/template-literals-expression.js
@@ -0,0 +1,16 @@
+/*
+
+Topic: Template Literals with Expressions
+*/
+
+// Code
+var a = 10;
+var b = 20;
+
+console.log(`Sum is ${a + b}`);
+
+// Output
+// Sum is 30
+
+// Reason
+// Expressions inside ${} get evaluated first
