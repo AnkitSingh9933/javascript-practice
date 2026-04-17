9eee5863b077b18f122391a9970fa7d10d2e860f
Ankit <138667092+AnkitSingh9933@users.noreply.github.com>
2026-04-17 09:06:05 +0530

    added JSON.parse example with explanation

---
+++ json/json-basic.js
@@ -0,0 +1,22 @@
+/*
+
+Topic: JSON Basics
+
+What I learned today:
+- JSON is a data format
+- Keys and values must be in double quotes
+*/
+
+// Code
+var jsonData = `{
+  "name": "Ankit",
+  "age": 27
+}`;
+
+console.log(jsonData);
+
+// Output
+// {"name":"Ankit","age":27}
+
+// Reason
+// JSON is stored as string format
